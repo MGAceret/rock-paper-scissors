d6ecee5a3c724903605e10244213b1334909d70b
Xyans Yes <mgaceret@gmail.com>
2026-05-15 22:33:54 +0800

    Created window prompt of inputing choice

--- script/script.js
+++ script/script.js
@@ -3,3 +3,10 @@ function getComputerChoice(option) {
 }
 
 console.log(getComputerChoice(3))
+
+function getHumanChoice(option) {
+    option = window.prompt("0 = Rock\n1 = Paper\n2 = Scissors");
+    return option
+}
+
+console.log(getHumanChoice())

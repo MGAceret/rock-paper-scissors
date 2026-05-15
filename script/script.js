05266eaa000f5d087e12bf57e533fb7c4e637b16
Xyans Yes <mgaceret@gmail.com>
2026-05-15 23:03:39 +0800

    Added global variable array for translating integers into strings

--- script/script.js
+++ script/script.js
@@ -1,18 +1,22 @@
+var option = ["Rock", "Paper", "Scissors"]
 // Opponent's choice
-function getComputerChoice(option) {
-    return "Computer chooses " + Math.floor(Math.random() * option)
+function getComputerChoice() {
+    const choice = Math.floor(Math.random() * 3)
+    // Translating values into string (RPS)
+    return "Computer chooses " + option[choice]
 }
 
-console.log(getComputerChoice(3))
+console.log(getComputerChoice())
 
 // User's choice
-function getHumanChoice(option) {
-    option = window.prompt("0 = Rock\n1 = Paper\n2 = Scissors");
-    return option
+function getHumanChoice() {
+    const choice = Number(window.prompt("0 = Rock\n1 = Paper\n2 = Scissors"))
+    return "User chooses " + option[choice]
 }
 
 console.log(getHumanChoice())
 
+// Scores
 var humanScore = 0
 var computerScore = 0
 

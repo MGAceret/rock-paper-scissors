73612b781c5cb75e196eefbf7471ad6faf7cff07
Xyans Yes <mgaceret@gmail.com>
2026-05-15 22:44:14 +0800

    Added global variable score and playround function. Additionally placed comments as headers

--- script/script.js
+++ script/script.js
@@ -1,12 +1,22 @@
+// Opponent's choice
 function getComputerChoice(option) {
     return "Computer chooses " + Math.floor(Math.random() * option)
 }
 
 console.log(getComputerChoice(3))
 
+// User's choice
 function getHumanChoice(option) {
     option = window.prompt("0 = Rock\n1 = Paper\n2 = Scissors");
     return option
 }
 
 console.log(getHumanChoice())
+
+var humanScore = 0
+var computerScore = 0
+
+// Round execution
+function playRound(humanChoice, computerChoice) {
+
+}
